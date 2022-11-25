b29a1159a4fa54e215bf3c5470fab9c7dbccf486
Anurag Patil <anurag3103@gmail.com>
2022-11-24 21:10:44 -0800

    added logos to public folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
 import Home from './pages';
 import About from './pages/about';
 import Services from './pages/services';
@@ -20,7 +19,6 @@ function App() {
         <Route path='/contact-us' component={Contact} />
         <Route path='/sign-up' component={SignUp} />
       </Routes>
-      <Sidebar />
     </Router>
   );
 }
---
+++ frontend/src/components/Sidebar/RightSideBar/right_sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
+import './right-sidebar.css';
+
+const Navbar = () => {
+  return (
+    <div className='App'>
+      <div classname='sidebar-container'>sb</div>
+      <div classname='sidebar-container'>bdy</div>
+    </div> 
+     
+  );
+};
+
+export default Navbar;
