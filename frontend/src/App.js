606389e7b8b4d2bc176eae800c5564befdd1a3d2
300177957 <yugam.arora@student.ufv.ca>
2022-11-24 20:09:24 -0800

    fixed the issue of app.css and made the dashboard.js as the primary page after authentication of user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Home from './pages';
+import Dashboard from './pages/Dashboard/dashboard';
 import About from './pages/about';
 import Services from './pages/services';
 import Contact from './pages/contact';
@@ -11,18 +10,16 @@ import SignUp from './pages/signup';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
+    <>
+    <Navbar />
       <Routes>
-        <Route path='/' exact component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/services' component={Services} />
+        <Route path='/' element ={<Dashboard />} />
+        <Route path='/about' element ={<About />} />
+        <Route path='/services' element ={<Services />} />
         <Route path='/contact-us' component={Contact} />
         <Route path='/sign-up' component={SignUp} />
       </Routes>
-      <Sidebar />
-    </Router>
+    </>
   );
 }
-
 export default App;
