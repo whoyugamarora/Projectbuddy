73b60fa6e8cc6b5114d29a866b8dc28960d4e037
whoyugamarora <arorayugam85@yahoo.com>
2024-12-16 00:31:18 -0800

    Changed the allowedhosts settings

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ app.use('/myaccount', skillRoutes);
 app.use('/idea', ideaRoutes);
 
 // Start the server
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
